aa012acce21c1418031fb67e1750796b3fda6e1f
Jonathan Adamski <jadamski@arbormetrix.com>
2019-07-17 17:22:13 -0400

    catch all address for deep linking.

--- client/index.js
+++ client/index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const proxy = require("http-proxy-middleware");
 const app = express();
 const port = 3000;
+const path = require("path");
 
 app.use(express.static("build"));
 
 app.use("/api", proxy({
     target: "http://api:8080"
 }));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./build/index.html"));
+});
 
 app.listen(port, () => {
     console.log('UI Server listening on port ' + port);
