d157aeac8b1e81a75236c308ad37c309daba32f6
Jonathan Adamski <jadamski@arbormetrix.com>
2019-07-18 23:28:16 -0400

    handling https swap in elb.

--- client/index.js
+++ client/index.js
@@ -2,25 +2,6 @@ const express = require("express");
 const proxy = require("http-proxy-middleware");
 const app = express();
 const path = require("path");
-const https = require("https");
-const http = require("http");
-const fs = require("fs");
-
-const env = require("dotenv");
-env.config();
-
-const pkPassphrase = process.env.PK_PASSPHRASE;
-
-app.enable("trust proxy");
-
-app.use((req, res, next) => {
-    if (req.secure) {
-        next();
-        return;
-    }
-
-    res.redirect("https://" + req.headers.host + req.url);
-});
 
 app.use(express.static("build"));
 
@@ -32,10 +13,4 @@ app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./build/index.html"));
 });
 
-http.createServer(app).listen(3000);
-
-https.createServer({
-    key: fs.readFileSync("certificate-private-key.txt"),
-    cert: fs.readFileSync("certificate-body.txt"),
-    passphrase: pkPassphrase
-}, app).listen(3443);
+app.listen(3000);
